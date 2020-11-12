78d1688379153ce1e50a87d112840cd56d9cce39
emk100 <elene.mika@gmail.com>
2020-11-12 14:08:33 -0800

    express, bodyparser, nodemon instalation

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/..client/dist'));
+const bodyparser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+let port = process.env.PORT || 3001;
+
+app.listen(port, ()=> {
+  console.log('listening on port', port);
+});
