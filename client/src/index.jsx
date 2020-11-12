95bf0f275832a1ca716458a68d794f5cb9878731
emk100 <elene.mika@gmail.com>
2020-11-12 13:52:21 -0800

    adding css file to react

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 
 import ReactDom from 'react-dom';
 import React, { useState } from 'react';
-
+import './CSS/Style.css';
 // eslint-disable-next-line func-style
 function Example () {
   // Declare a new state variable, which we'll call "count"
@@ -10,7 +10,7 @@ function Example () {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={() => setCount(count + 1)} className = "row">
         Click me
       </button>
     </div>
