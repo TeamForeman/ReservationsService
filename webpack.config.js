9a9c826e34413cf784e6de501db223c67e5faaef
emk100 <elene.mika@gmail.com>
2020-11-12 11:54:02 -0800

    add webpack-babel config files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+module.exports = {
+
+  entry: SRC_DIR +'/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
