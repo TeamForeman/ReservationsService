0d90461f569a088677bf57f96598a9fd93314979
emk100 <elene.mika@gmail.com>
2020-11-12 20:30:54 -0800

    DB and data population

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 let app = express();
-
 app.use(express.static(__dirname + '/..client/dist'));
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+
+let db = require('../DB/db.js');
+let data = require('../DB/data.js');
 
 let port = process.env.PORT || 3001;
 
