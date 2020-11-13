11c9c17bf43a4ba19c28798af3166fa17cb39f85
emk100 <elene.mika@gmail.com>
2020-11-13 14:56:17 -0800

    calendar and datepicker install

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 
 import ReactDom from 'react-dom';
 import React, { useState } from 'react';
+import Calendar from './Calendar/Calendar.jsx';
 import './CSS/Style.css';
 // eslint-disable-next-line func-style
 function Example () {
@@ -9,6 +10,7 @@ function Example () {
 
   return (
     <div>
+      <Calendar/>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)} className = "row">
         Click me
