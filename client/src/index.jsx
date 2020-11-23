a1f61034bb8eb1994cae9052d291664f092f3d59
emk100 <elene.mika@gmail.com>
2020-11-23 15:40:18 -0800

    end

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -117,7 +117,7 @@ function Example () {
       params: query
     }).then (data => {
       console.log('enddate data  received');
-      console.log('enddate data  shoeFees', endDate.getTime() - startDate.getTime());
+      console.log('enddate data  shoeFees', endDate.getTime() - startDate.getTime() / (1000 * 60 * 60 * 24));
       if (nights >= 1) {
         let receivedObj = data.data[0];
 
